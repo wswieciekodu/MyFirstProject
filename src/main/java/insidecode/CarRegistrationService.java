2875f1540dcbb1ce77417184b1f7c3de7c7601ab
Magda <wswieciekodu@gmail.com>
2024-01-18 17:18:54 +0100

    #2 Add user

---
+++ src/main/java/insidecode/CarRegistrationService.java
@@ -0,0 +1,28 @@
+package insidecode;
+
+import insidecode.model.Car;
+import insidecode.model.Owner;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+public class CarRegistrationService {
+    private CarDatabase carDataBase;
+    private CarNumberGenerator carNumberGenerator;
+
+    public String registerCar(Owner person, Car car) {
+        String number = carNumberGenerator.generateNumber(person.getAddress().getState());
+
+        // add user
+
+        carDataBase.add(person, car, number);
+
+        return number;
+    }
+
+    public List<Car> getCars(Owner person) {
+
+        return carDataBase.getCars(person);
+    }
+}
