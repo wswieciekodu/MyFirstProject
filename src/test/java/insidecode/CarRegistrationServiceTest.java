edf113164dc4cffee1fd9bc31ef95afcfadcff89
Magda <wswieciekodu@gmail.com>
2024-01-24 18:16:38 +0100

    #3 Code Cleanup

---
+++ src/test/java/insidecode/CarRegistrationServiceTest.java
@@ -0,0 +1,15 @@
+package insidecode;
+
+import org.junit.jupiter.api.Test;
+
+class CarRegistrationServiceTest {
+
+    @Test
+    void registerCar() {
+
+    }
+
+    @Test
+    void getCars() {
+    }
+}
